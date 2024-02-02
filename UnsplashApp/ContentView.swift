34faf333bb8d8cca40e077270449b685a603733e
Raphael Beaudet <raphaelbeaudet@icloud.com>
2024-02-02 15:21:14 +0100

    image button loading

--- UnsplashApp/ContentView.swift
+++ UnsplashApp/ContentView.swift
@@ -6,19 +6,181 @@
 //
 
 import SwiftUI
+import Foundation
 
 struct ContentView: View {
+    @State private var imageURL: URL?
+    @State var imageList: [UnsplashPhoto] = []
+    
+    func loadData() async {
+        // Créez une URL avec la clé d'API
+        let url = URL(string: "https://api.unsplash.com/photos?client_id=\(ConfigurationManager.instance.plistDictionnary.clientId)")!
+
+        do {
+            // Créez une requête avec cette URL
+            let request = URLRequest(url: url)
+            
+            // Faites l'appel réseau
+            let (data, response) = try await URLSession.shared.data(for: request)
+            
+            // Transformez les données en JSON
+            let deserializedData = try JSONDecoder().decode([UnsplashPhoto].self, from: data)
+
+            // Mettez à jour l'état de la vue
+            imageList = deserializedData
+
+        } catch {
+            print("Error: \(error)")
+        }
+    }
+        
+    let imageURLs: [String] = [
+        "https://images.unsplash.com/photo-1683009427666-340595e57e43?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MXwxfGFsbHwxfHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1563473213013-de2a0133c100?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHwyfHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1490349368154-73de9c9bc37c?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHwzfHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1495379572396-5f27a279ee91?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw0fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1560850038-f95de6e715b3?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw1fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1695653422715-991ec3a0db7a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MXwxfGFsbHw2fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1547327132-5d20850c62b5?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw3fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1492724724894-7464c27d0ceb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw4fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1475694867812-f82b8696d610?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHw5fHx8fHx8MXx8MTcwMzc1OTU1MXw&ixlib=rb-4.0.3&q=80&w=1080",
+        "https://images.unsplash.com/photo-1558816280-dee9521ff364?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w1MzYyNDN8MHwxfGFsbHwxMHx8fHx8fDF8fDE3MDM3NTk1NTF8&ixlib=rb-4.0.3&q=80&w=1080"
+    ]
+    
+    let columns = [
+        GridItem(.flexible()),
+        GridItem(.flexible())
+    ]
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // le bouton va lancer l'appel réseau
+            Button(action: {
+                Task {
+                    await loadData()
+                }
+            }, label: {
+                Text("Load Data")
+            })
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(imageURLs, id: \.self) { image in
+                        AsyncImage(url: URL(string: image)!) { image in
+                            image.centerCropped()
+                        }
+                        placeholder: {
+                            ProgressView()
+                        }
+                        .frame(
+                            width: 150,
+                            height: 150,
+                            alignment: .center
+                        )
+                    }
+                }.padding(.horizontal, 40)
+            }
         }
-        .padding()
+        
+        /*VStack {
+            if let imageURL = imageURL {
+                AsyncImage(url: imageURL) { phase in
+                    switch phase {
+                    case .empty:
+                        ProgressView()
+                    case .success(let image):
+                        image
+                            .resizable()
+                            .scaledToFit()
+                    case .failure:
+                        Image(systemName: "exclamationmark.triangle")
+                            .resizable()
+                            .scaledToFit()
+                            .foregroundColor(.red)
+                    @unknown default:
+                        EmptyView()
+                    }
+                }
+                .frame(width: 200, height: 200) // Adjust the frame size as needed
+            } else {
+                Text("Loading...")
+                    .onAppear {
+                        // Call your API and parse the JSON response
+                        fetchDataFromAPI()
+                    }
+            }
+        }*/
+        
+        /*func fetchDataFromAPI() {
+            guard let url = URL(string: "https://api.unsplash.com/photos/?client_id=c5XZtslBv0KMmX3d7JBAv16tYODW2gG20l4voifKqv0") else {
+                return
+            }
+
+            URLSession.shared.dataTask(with: url) { data, response, error in
+                guard let data = data, error == nil else {
+                    print("Error fetching data: \(error?.localizedDescription ?? "Unknown error")")
+                    return
+                }
+
+                do {
+                    let decoder = JSONDecoder()
+                    let result = try decoder.decode([ImageInfo].self, from: data)
+                    
+                    if let firstImage = result.first, let imageURLString = firstImage.urls?.regular {
+                        self.imageURL = URL(string: imageURLString)
+                    }
+                } catch {
+                    print("Error decoding JSON: \(error.localizedDescription)")
+                }
+            }.resume()
+        }*/
     }
 }
 
-#Preview {
-    ContentView()
+extension Image {
+    func centerCropped() -> some View {
+        GeometryReader { geo in
+            self
+            .resizable()
+            .scaledToFill()
+            .frame(width: geo.size.width, height: geo.size.height)
+            .clipped()
+            .cornerRadius(12)
+        }
+    }
 }
+
+struct ImageInfo: Decodable {
+    let urls: ImageURLs?
+}
+
+struct ImageURLs: Decodable {
+    let regular: String?
+}
+
+
+import Foundation
+
+// MARK: - WelcomeElement
+struct UnsplashPhoto: Codable {
+    let id, slug: String
+    let urls: Urls
+    let user: User
+}
+
+// MARK: - Urls
+struct Urls: Codable {
+    let raw, full, regular, small: String
+    let thumb, smallS3: String
+
+    enum CodingKeys: String, CodingKey {
+        case raw, full, regular, small, thumb
+        case smallS3 = "small_s3"
+    }
+}
+
+// MARK: - User
+struct User: Codable {
+    let name: String
+}
+
+typealias Photo = [UnsplashPhoto]
