d11ac7020e054aab8410162ef19e17a00f88f69c
Raphael Beaudet <raphaelbeaudet@icloud.com>
2024-02-02 09:03:02 +0100

    Initial Commit

---
+++ UnsplashApp/UnsplashAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashAppApp.swift
+//  UnsplashApp
+//
+//  Created by Raphaël Beaudet on 02/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
